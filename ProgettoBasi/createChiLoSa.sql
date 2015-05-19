07c35d48c5e4d625e0e3705d1899bb3768c23507
Luni-4 <Luni-4@users.noreply.github.com>
2015-05-19 19:52:11 +0200

    SQL completo

--- ProgettoBasi/createChiLoSa.sql
+++ ProgettoBasi/createChiLoSa.sql
@@ -1,102 +1,106 @@
 CREATE TABLE Utente
-	(nome VARCHAR(20) PRIMARY KEY,
-	 email VARCHAR(20) NOT NULL UNIQUE,
+	(nome VARCHAR(20) NOT NULL,
+	 email VARCHAR(20) NOT NULL,
 	 password VARCHAR(16) NOT NULL,
 	 residenza VARCHAR(20), --opzionale
 	 tipo VARCHAR(7) NOT NULL, -- il tipo  è normale/vip-> max_length=7
-	 datanascita DATE
+	 datanascita DATE,
+	 PRIMARY KEY(nome),
+	 UNIQUE(email)
 	 );
 
 CREATE TABLE Categoria
-	(nomec VARCHAR(20) PRIMARY KEY
+	(nomec VARCHAR(20) NOT NULL,
+	 PRIMARY KEY(nomec)
 	);
 
 CREATE TABLE Sottocategoria
 	(
-	nomesc VARCHAR(20),
-	categoria REFERENCES Categoria(nomec)
+	nomesc VARCHAR(20) NOT NULL,
+	nomec  VARCHAR(20) NOT NULL  REFERENCES Categoria(nomec)
 				ON DELETE RESTRICT
 				ON UPDATE RESTRICT, --non permetto cambiamenti alle categoria per evitare grandi effetti cascata, guarda sotto
-	PRIMARY KEY(nomesc,categoria)
+	PRIMARY KEY(nomesc,nomec)
 	);
 	
 CREATE TABLE Preferenza
-	(utentep VARCHAR(20) REFERENCES Utente(nome)
+	(nome VARCHAR(20) NOT NULL REFERENCES Utente(nome)
 						ON DELETE CASCADE --se tolgo un utente dal db la sua preferenza scompare
 						ON UPDATE CASCADE,
-	categoriap VARCHAR(20) REFERENCES Categoria(nomec)
+	nomec VARCHAR(20) NOT NULL REFERENCES Categoria(nomec)
 						ON DELETE RESTRICT --non permetto di cambiare nome alla categoria, avrei troppi effetti cascata in altri punti del progetto
 						ON UPDATE RESTRICT, --es se un utente ha messo preferenza "cucina" io non posso cambiare il nome alla categoria e mettergli "pettinare_giraffe"
-	PRIMARY KEY (utentep,categoriap)
+	PRIMARY KEY (nome,nomec)
 	);
 	
 CREATE TABLE Domandaperta
-	(utented VARCHAR(20) REFERENCES Utente(nome)
-						ON DELETE SET DEFAULT Utente(nome)+'cancellato' --ci teniamo anche le domande senza utente per fare una sorta di history?
-						ON UPDATE CASCADE, --per esempio se l'utente ha bisogno di cambiare o username o pass
+	(idd int NOT NULL AUTO_INCREMENT,						
 	datad DATETIME DEFAULT CURRENT_TIMESTAMP,
 	testo TEXT NOT NULL,
 	imgurl VARCHAR(50),
 	imgtesto TEXT,
 	chiusa BOOLEAN DEFAULT FALSE,-- quando un utente viene eliminato questo campo diventa true.
-	PRIMARY KEY (utented,datad)
+	nome VARCHAR(20) REFERENCES Utente(nome)
+				ON DELETE SET NULL,
+				ON UPDATE CASCADE,
+	PRIMARY KEY (idd)
 	);
 
 CREATE TABLE Sondaggio
-	(utentes VARCHAR(20) REFERENCES Utente(nome)
-						ON DELETE SET DEFAULT utentes+'cancellato' --ci teniamo anche le domande senza utente per fare una sorta di history?
-						ON UPDATE CASCADE, --per esempio se l'utente ha bisogno di cambiare o username o pass
-	datas DATETIME DEFAULT CURRENT_TIMESTAMP,
+	(idd int NOT NULL AUTO_INCREMENT,						
+	datad DATETIME DEFAULT CURRENT_TIMESTAMP,
 	testo TEXT NOT NULL,
 	imgurl VARCHAR(50),
 	imgtesto TEXT,
-	chiusa BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY (utentes,datas)
+	chiusa BOOLEAN DEFAULT FALSE,-- quando un utente viene eliminato questo campo diventa true.
+	nome VARCHAR(20) REFERENCES Utente(nome)
+				     ON DELETE NULL,
+				     ON UPDATE CASCADE,
+	PRIMARY KEY (idd)
 	);
 	
 CREATE TABLE Topic1
-		(categoriat1 VARCHAR(20) references Categoria(nomec)
-						ON DELETE RESTRICT
-						ON UPDATE RESTRICT,
-		nomet1 VARCHAR(20),
-		datat1 DATETIME,
-		FOREIGN KEY(nomet1,datat1) references Domandaperta(utented,datad) 
-		PRIMARY KEY (categoriat1,nomet1,datat1)
+		(nomec VARCHAR(20) NOT NULL REFERENCES Categoria(nomec)
+						   ON DELETE RESTRICT
+						   ON UPDATE RESTRICT,
+		idd int NOT NULL REFERENCES Domandaperta(idd)
+	                     ON DELETE CASCADE,				       
+		PRIMARY KEY (nomec,idd)
 		);
 		
 CREATE TABLE Topic2
-		(categoriat2 VARCHAR(20) references Categoria(nomec)
-						ON DELETE RESTRICT
-						ON UPDATE RESTRICT,
-		nomet2 VARCHAR(20),
-		datat2 DATETIME,
-		FOREIGN KEY(nomet2,datat2) references Sondaggio(utentes,datas) 
-		PRIMARY KEY (categoriat2,nomet2,datat2)
+		(nomec VARCHAR(20) NOT NULL REFERENCES Categoria(nomec)
+						   ON DELETE RESTRICT
+						   ON UPDATE RESTRICT,
+		idd int NOT NULL REFERENCES Sondaggio(idd)
+	                     ON DELETE CASCADE,				       
+		PRIMARY KEY (nomec,idd)
 		);
 		
 CREATE TABLE Rispostapredefinita
-		(utenterispostap REFERENCES Utente(nome)
-				ON DELETE SET NULL
-				ON UPDATE CASCADE,
-		 utentesondaggio VARCHAR(20), 
-	     datasondaggio DATETIME,
+		(idr int NOT NULL AUTO_INCREMENT,	
 		 anonimo BOOLEAN DEFAULT FALSE,
-		 testorisp VARCHAR(30) NOT NULL,
-		 FOREIGN KEY(utentesondaggio,datasondaggio) references Sondaggio(utentes,datas),
-		 PRIMARY KEY(utenterispostap,utentesondaggio,datasondaggio)
+		 datar DATETIME DEFAULT CURRENT_TIMESTAMP,
+		 testorisp TEXT NOT NULL,	
+		 nome VARCHAR(20) REFERENCES Utente(nome)
+				ON DELETE SET NULL,
+				ON UPDATE CASCADE,
+		idd int NOT NULL REFERENCES Sondaggio(idd)
+	                     ON DELETE CASCADE,			 
+		 PRIMARY KEY(idr)
 		);
 		
 CREATE TABLE Rispostaperta
-		(utenterispostaperta REFERENCES Utente(nome)
-				ON DELETE SET NULL
+		(idr int NOT NULL AUTO_INCREMENT,	
+		 anonimo BOOLEAN DEFAULT FALSE,
+		 datar DATETIME DEFAULT CURRENT_TIMESTAMP,
+		 testorisp TEXT NOT NULL,
+		 votopositivo LONG DEFAULT 0,
+		 votonegativo LONG DEFAULT 0,
+		 nome VARCHAR(20) REFERENCES Utente(nome)
+				ON DELETE SET NULL,
 				ON UPDATE CASCADE,
-		 datara DATETIME DEFAULT CURRENT_TIMESTAMP,
-		 testora TEXT NOT NULL,
-		 anonimo BOOLEAN DEFAULT FALSE,		
-		 votopos LONG DEFAULT 0,
-		 votoneg LONG DEFAULT 0,
-		 utentedomandaperta VARCHAR(20), 
-	     datadomandaperta DATETIME,
-		 FOREIGN KEY(utentedomandaperta,datadomandaperta) references Domandaperta(utented,datad),
-		 PRIMARY KEY(utenterispostaperta,datara)
+		idd int NOT NULL REFERENCES Domandaperta(idd)
+	                     ON DELETE CASCADE,			 
+		 PRIMARY KEY(idr)		
 		);
